131beab303a9ef2a0edb735c283ba6500a969987
Runniersoaoi <matiasaquinozc@gmail.com>
2025-01-02 19:19:36 -0500

    feat: change detail in routes

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
             <Navbar />
             <main className="flex-grow">
               <Routes>
-                <Route path="/" element={<Home />} />
+                <Route path="/capiflix" element={<Home />} />
                 <Route path="/create" element={<CreateElement />} />
               </Routes>
             </main>
