6fe6209f2f1c4ea8d5e8d72e12f6358deb58c3aa
Runniersoaoi <matiasaquinozc@gmail.com>
2025-01-03 10:03:54 -0500

    feat: change detail in routes

--- src/components/Fotter.jsx
+++ src/components/Fotter.jsx
@@ -2,9 +2,7 @@ const Footer = () => {
   return (
     <footer className="bg-[#714017] text-white py-4">
       <div className="container mx-auto px-4 text-center">
-        <p className="text-sm sm:text-base">
-          &copy; 2023 MultimediaCRUD. Todos los derechos reservados.
-        </p>
+        <p className="text-sm sm:text-base">&copy; 2025 CapiFlix</p>
       </div>
     </footer>
   );
